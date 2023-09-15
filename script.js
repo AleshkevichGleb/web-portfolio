1006d8e306e3aecf143c145f34d057a8fbdc5d94
Gleb <aleshkevichgleb05@gmail.com>
2023-09-15 13:44:30 +0300

    add: chapter about and contacts

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('.header__link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
