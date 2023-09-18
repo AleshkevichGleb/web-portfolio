69f37503162b8550f0db61d6de856ef9e6128920
Gleb <aleshkevichgleb05@gmail.com>
2023-09-18 10:24:46 +0300

    add: preloader

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let i = 0;
+
+let interval = setInterval(() => {
+    i+= Math.ceil(Math.random()* 20)
+    percents.innerHTML = i;
+}, 400)
+
+
+setTimeout(() => {
+    preloader.classList.add('preloader--hide');
+    percents.innerHTML = 100;
+    clearInterval(interval)
+    document.body.classList.remove('body')
+}, 2000)
 
 document.querySelectorAll('.scroll-link').forEach(link => {
     link.addEventListener('click', function (e) {
